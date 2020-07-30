ddc06cbadc6bf74b63281c4094a78660a12964bc
xanpj <this@mail.com>
2020-07-31 01:03:34 +0200

    features(extensions): added (v2) panel and image-selection
    - panel history working with native history
    - panel is fully synced to canvas on changes instead of per-command-syncing.
    this simplifies the code without significant speed loss
    - improved postit-js compatibility in old extensions

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ import DragDropImagesModule from './lib/features/drag-drop-images';
 
 import SelectionOrganizerModule from './lib/features/selection-organizer';
 
-import PropertiesPanelModule from './lib/features/properties-panel';
+import PropertiesPanelModule from './lib/features/properties-panel-v2';
+
+import ImageSelectionV2Module from './lib/features/image-selection-v2';
 
 export default {
   __depends__: [
     DragDropImagesModule,
     SelectionOrganizerModule,
     PropertiesPanelModule,
+    ImageSelectionV2Module,
   ],
   DragDropImages: DragDropImagesModule,
   SelectionOrganizer: SelectionOrganizerModule,
   PropertiesPanel: PropertiesPanelModule,
+  ImageSelectionV2Module: ImageSelectionV2Module,
 };
+
